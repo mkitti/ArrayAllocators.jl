52465ccc188d61091c03f2f0c6fc61a0cdaeae01
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2022-04-13 20:08:42 -0400

    Create a posix_memalign julia function

--- src/POSIX.jl
+++ src/POSIX.jl
@@ -8,10 +8,18 @@ abstract type AbstractMemAlign{B} <: LibcArrayAllocator{B} end
 
 # Copied from https://github.com/JuliaPerf/BandwidthBenchmark.jl/blob/main/src/allocate.jl
 # Copyright (c) 2021 Carsten Bauer <crstnbr@gmail.com> and contributors
+# Originating from https://discourse.julialang.org/t/julia-alignas-is-there-a-way-to-specify-the-alignment-of-julia-objects-in-memory/57501/2
+# Copyright (c) 2021 Steven G. Johnson
+
 function check_alignment(alignment)
     ispow2(alignment) || throw(ArgumentError("$alignment is not a power of 2"))
     alignment ≥ sizeof(Int) || throw(ArgumentError("$alignment is not a multiple of $(sizeof(T))"))
     return nothing
+end
+
+function posix_memalign(ptr, alignment, num_bytes)
+    err = ccall(:posix_memalign, Cint, (Ref{Ptr{T}}, Csize_t, Csize_t), ptr, alignment, num_bytes)
+    return err
 end
 
 """
@@ -41,20 +49,9 @@ MemAlign(alignment) = MemAlign{DefaultByteCalculator}(alignment)
 function allocate(alloc::MemAlign{B}, ::Type{T}, num_bytes) where {B, T}
     isbitstype(T) || throw(ArgumentError("$T is not a bitstype"))
     p = Ref{Ptr{T}}()
-    err = ccall(:posix_memalign, Cint, (Ref{Ptr{T}}, Csize_t, Csize_t), p, alloc.alignment, num_bytes)
+    err = posix_memalign(p, alloc.alignment, num_bytes)
     iszero(err) || throw(OutOfMemoryError())
     return p[]
 end
-
-#=
-function (::Type{ArrayType})(alloc::MemAlign{B}, dims) where {T, B, ArrayType <: AbstractArray{T}}
-    isbitstype(T) || throw(ArgumentError("$T is not a bitstype"))
-    p = Ref{Ptr{T}}()
-    n = nbytes(B{T}(dims))
-    err = ccall(:posix_memalign, Cint, (Ref{Ptr{T}}, Csize_t, Csize_t), p, alloc.alignment, n)
-    iszero(err) || throw(OutOfMemoryError())
-    return wrap_libc_pointer(p[], dims)
-end
-=#
 
 end # module POSIX
